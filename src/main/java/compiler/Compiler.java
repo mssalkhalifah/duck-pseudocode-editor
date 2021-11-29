896da282e34e2b79729906ff3316b61ad6a0a675
mssalkhalifah <mssalkhalifah@gmail.com>
2021-11-29 16:32:00 +0300

    Added a CLR parser

--- src/main/java/compiler/Compiler.java
+++ src/main/java/compiler/Compiler.java
@@ -4,17 +4,34 @@ import compiler.exceptions.scanner.UnrecognizedCharacterException;
 import compiler.lexer.Lexer;
 import compiler.lexer.Lexer.Token;
 import compiler.lexer.Lexer.TokenType;
+import compiler.parser.GrxParser;
 
 import java.io.IOException;
 import java.io.StringReader;
+import java.util.HashMap;
 
 public final class Compiler {
     private final Lexer lexer;
+    private final GrxParser parser;
     private final StringBuilder lexerLogger;
+    private final HashMap<TokenType, Integer> tokenTypeMap;
 
     public Compiler(String sourceProgram) {
         this.lexer = new Lexer(new StringReader(sourceProgram));
+        this.parser = new GrxParser();
         this.lexerLogger = new StringBuilder();
+
+        this.tokenTypeMap = new HashMap<>();
+        tokenTypeMap.put(TokenType.SEMICOL, 0);
+        tokenTypeMap.put(TokenType.STR, 1);
+        tokenTypeMap.put(TokenType.FLOAT, 2);
+        tokenTypeMap.put(TokenType.NUM_CONST, 3);
+        tokenTypeMap.put(TokenType.COLON, 4);
+        tokenTypeMap.put(TokenType.ENDINPUT, 5);
+        tokenTypeMap.put(TokenType.ID, 6);
+        tokenTypeMap.put(TokenType.ASSIGN, 7);
+        tokenTypeMap.put(TokenType.STARTINPUT, 8);
+        tokenTypeMap.put(TokenType.INT, 9);
     }
 
     public String getLexerLogs() {
@@ -23,14 +40,25 @@ public final class Compiler {
 
     public void compile() {
         try {
-            Token token = lexer.yylex();
-            lexerLogger.append(token).append('\n');
-            while (token.getType() != TokenType.ENDINPUT) {
+            Token token;
+
+            do {
                 token = lexer.yylex();
                 lexerLogger.append(token).append('\n');
-            }
-        } catch (IOException | UnrecognizedCharacterException e) {
+                parser.parse(tokenTypeMap.get(token.getType()), token);
+            } while (token.getType() != TokenType.ENDINPUT);
+        } catch (IOException
+                | UnrecognizedCharacterException
+                | RuntimeException e) {
             e.printStackTrace();
+        }
+    }
+
+    private void getTokenType(int tokenType) {
+        switch (tokenType) {
+            case 0: {
+                
+            }
         }
     }
 }
---
+++ src/test/java/tests/parser/ParserTest.java
@@ -0,0 +1,28 @@
+package tests.parser;
+
+import compiler.Compiler;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class ParserTest {
+    private static Compiler compiler;
+
+    @BeforeAll
+    public static void setup() throws IOException {
+        String sourceProgram = Files
+                .readString(Path
+                        .of("/home/moh/Dev/duck-pseudocode-editor/src/test/java/tests/parser/source_program"));
+        System.out.println("Source Program:\n" + sourceProgram);
+        compiler = new Compiler(sourceProgram);
+    }
+
+    @Test
+    public void parseTest() {
+        compiler.compile();
+        System.out.println("\n" + compiler.getLexerLogs());
+    }
+}
